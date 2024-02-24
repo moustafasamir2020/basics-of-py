b7d970c75e6e8b8a02053adb6c8a2d8a38979810
moustafasamir2020 <moustafasamir2020@gmail.com>
2024-02-25 00:19:53 +0200

    first commit

---
+++ labb.py
@@ -0,0 +1,20 @@
+#print('hello moustafa')
+#print('hello moustafa')
+#print('hello ' + 'omar')
+#print('hello omar ' + '4')
+#print('hello mostafa','5555')
+
+
+#input('enter your name :')
+#print('hello ',input('enter your name :'))
+
+#print ('hallo ',input('enter your name :'),'your age is', input('enter your age:'))
+#input('email please:')
+#print('welcom to our website')/
+
+# print('welcom')
+#input('did you the first one her!?')
+#print('hallo',input('enter your name ?','your age ?'))
+print('hello moustafa')
+      
+#input('enter your name ?','your age ?')
