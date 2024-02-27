cacf8d5f32555adc6f09aa42c3cdfeb26e8aac47
moustafasamir2020 <moustafasamir2020@gmail.com>
2024-02-27 20:57:15 +0200

    'moustafa store

--- basics.py
+++ basics.py
@@ -1,3 +1,8 @@
-print('stay here')
-print('how old are you?')
-print('hallo', input('put your name?'), 'your age is ?', input('enter yur age:'))
+print('welcom to our project')
+input('can you help us to know who are you?')
+print('ok lets go .')
+input('what is your name?')
+input('your age please')
+print('hallo sir ')
+print('this project can help you to study hard ',input('what can i do to help you in your study?'),'we can call you in your number',input('enter your phone'))
+#dont forgit run your app
