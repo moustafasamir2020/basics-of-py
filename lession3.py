f20f6b6a605cee1055d7a4cdcc0dc41dc37426cd
moustafasamir2020 <moustafasamir2020@gmail.com>
2024-03-08 15:32:05 +0200

    up

---
+++ lession3.py
@@ -0,0 +1,27 @@
+# grade = int(input('enter your grade:'))
+# if grade>=90 :
+#     print('excellent')
+# elif 90<grade>=80:
+#     print('very good')
+# elif 80<grade>=70:
+#     print('good')
+# elif 70<grade>=60:
+#     print('pass')
+# else :print('falid')
+# ---------------------------------------------------------------------------------------------------------------------
+num1=float(input('first number:'))
+num2=float(input('sacend number:'))
+ope=input('choose from this ope +-/* // ** %:')
+
+if ope== '+':
+    print(num1+num2)
+elif ope== '-':
+  minus=  input('choose from num1-num2 or num2-num1 1 or 2:')
+  if minus ==1 :
+    print(num1-num2)
+  else:print(num2-num1)
+elif ope == '/':
+   print(num1/num2)
+elif ope == '*':
+   print(num1*num2)
+else: print('choose please')
