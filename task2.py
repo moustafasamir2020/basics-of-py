9440a4589242270e92119a179c34963469186ed7
moustafasamir2020 <moustafasamir2020@gmail.com>
2024-03-10 01:09:20 +0200

    task2

---
+++ task2.py
@@ -0,0 +1,40 @@
+# # q8)-------------->
+# print ('welcom to our company')
+# print ('welcom',input('enter your name: '),'your code is', input('enter your code :'))
+# unit=int(input('enter your units:'))
+# if unit<=100:
+#     print('your total bill amount did not exceed 100 unit')
+# if 100<unit<=200:
+#     print('your total bill amount is ',(unit-100)*5)
+# else:
+#     print('your total bill amount is ',((unit-200)*10)+500)
+#  # ------------------------------------------------------------------------>
+# # q1)------------->
+# print('e.f.s school')
+# print("we wish you a best wishes")
+# input('what is your name?')
+# grade=int(input("your marks please:"))
+# if grade> 90:
+#     print("your grade is (A)")
+# elif 90>=grade>80:
+#     print("your grade is (B)")
+# elif 80>=grade>=60:
+#     print('your grade is (c)')
+# else:print('your grade is (c)')
+# # ----------------------------------------------------------------------------------->
+# q2)------------------>
+# print('welcome sir we pleased you being with us  ')
+# print("you can call the customer service if you have any problem ")
+# print('your bike type is  ',input('enter your type of your bike:'))
+# price=int(input('enter the cost of your bike:'))
+# if price>100000:
+#     print("your tax is ",price*(15/100))
+# elif 50000<price<=100000:
+#     print('your tax is ', price*(10/100))
+# else: print('your tax is ', price*(5/100))
+# print('thank you for help us')
+# # ----------------------------------------------------------------------------------->
+#q5)
+
+
+
